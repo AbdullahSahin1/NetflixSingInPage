ba858750068e277b308e7e143b964cbecc7e284f
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-02-13 01:14:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ const inputOnBlur = (ev) =>{
         }
     }
 }
+const inputOnFocus = (ev) =>{
+    inputTouched[ev.name] = true;
+}
+
+const validateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validatePhone = email => {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return re.test(String(email).toLowerCase());
+}

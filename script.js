a6810d463654df369e28a13b323a664ac22ded96
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-02-13 00:28:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let inputTouched = {
+    email: false,
+    password: false
+}
 
+const inputEmail = document.getElementById("inputEmail")
+const inputPassword = document.getElementById("inputPassword")
+const inputWrapperEmail = document.getElementById("input-wrapper-email")
+const inputWrapperPassword = document.getElementById("input-wrapper-password")
+const warningEmail = document.getElementById("warningEmail")
+const warningPassword = document.getElementById("warningPassword")

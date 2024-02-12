be7eb416a40f28a601cef435a32976698c162991
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-02-13 00:49:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ const inputWrapperEmail = document.getElementById("input-wrapper-email")
 const inputWrapperPassword = document.getElementById("input-wrapper-password")
 const warningEmail = document.getElementById("warningEmail")
 const warningPassword = document.getElementById("warningPassword")
+
+const inputOnBlur = (ev) =>{
+    if(inputTouched.email){
+        if(!validateEmail(inputEmail.value) && !validatePhone(inputEmail.value)){
+            warningEmail.style.display="block"
+            inputEmail.style.borderBottom='2px solid #e87c03'
+        }
+        else{
+            warningEmail.style.display="none"
+            inputEmail.style.borderBottom="none"
+        }
+    }
+    if(inputTouched.password){
+        if(!(inputPassword.value.length >= 4 && inputPassword.value.length <= 60)){
+            warningPassword.style.display="block"
+            inputPassword.style.borderBottom='2px solid #e87c03'
+        }
+        else{
+            warningPassword.style.display="none"
+            inputPassword.style.borderBottom="none"
+        }
+    }
+}
